1f799de40aab8a5d8b1317e11720b860b7cc42c8
Madhurchhetri <tandonnakul987@gmail.com>
2025-11-06 22:13:33 +0530

    day11 of JS

---
+++ Day11/script.js
@@ -0,0 +1,91 @@
+// function aalooPratha(){
+//     console.log("🫓🫓");
+    
+// }
+// aalooPratha()
+// aalooPratha()
+
+
+// ..............parameter and arguments..........
+
+// function abcd(name ,age){
+//                 // parameter
+// }
+// abcd("madhur",25)
+// // argument
+
+// ..................rest parameter..........
+
+// function abcd(a,b,...c){
+//  console.log(a,b,c);
+ 
+// }
+// abcd(1,2,3,4,5,6,7,8)
+
+// ................hoisting.............
+
+// abcd();
+// function abcd(){
+//     console.log('hey');
+    
+// }
+// abcd()
+// var abcd = function(){
+//     console.log('hello');
+    
+// }
+
+// op : error function is not defined
+
+// ...............parameter in javascript ['required','destrucred','rest', 'default']..............
+
+// function abcd (a,b,c){
+//     console.log(a , b , c);
+    
+// }
+// abcd()
+
+// function abcd ({name,age}){
+//     console.log(name,age);
+    
+// }
+// abcd({name:"madhur",age:25})
+
+// default
+
+// function abcd(a,b,c =0){
+//     console.log(a,b,c);
+    
+// }
+// abcd(1,2)
+
+
+// fat arrow function:
+
+// let abc =()=>{
+//     console.log('hello');
+    
+// }
+// abc()
+
+// anonymous function:
+
+// let abcd = function(){
+
+// }
+// abcd()
+
+// higher order function: koi bhi  aise function jo ek or function return karta ho  ya fir argument function leta ho
+
+// function abcd(){
+//     return function(){
+
+//     }
+// }
+
+// function abcd(a){
+
+// }
+// abcd(function(){
+
+// })
